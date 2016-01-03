a309f9bd22c89495667a8708c1c0c85ba4a06c77
Luciano Neucamp <lucianoneucamp@gmail.com>
2016-01-03 15:42:38 -0200

    test fail

---
+++ test/dummy-fail.spec.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var should = require('should');
+
+
+describe('test', function () {
+  it('should be ok', function () {
+      "not".should.be.equal('ok')
+  });
+});
---
+++ test/dummy.spec.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var should = require('should');
+
+
+describe('test', function () {
+  it('should be ok', function () {
+      "ok".should.be.equal('ok')
+  });
+});
